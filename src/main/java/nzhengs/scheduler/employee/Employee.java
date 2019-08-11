a85b834b27aaeb6af9d435a897396172caa67cda
Anjana Hengaju <an.hengaju@gmail.com>
2019-08-11 16:37:07 -0500

    Getter and Setter

--- src/main/java/nzhengs/scheduler/employee/Employee.java
+++ src/main/java/nzhengs/scheduler/employee/Employee.java
@@ -8,17 +8,94 @@ import javax.persistence.Id;
 public class Employee {
     @Id
     @GeneratedValue
-    public Long id;
-    public String firstName;
-    public String middleName;
-    public String lastName;
-    public String address1;
-    public String address2;
-    public String city;
-    public String state;
-    public String zip;
-    public Gender gender;
-    public String phone;
+    private Long id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    private String zip;
+    private Gender gender;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 }
