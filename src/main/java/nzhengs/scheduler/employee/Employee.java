9f359fb26f30949eb2450dd4e9fd63e08d7938bb
Anjana Hengaju <an.hengaju@gmail.com>
2019-08-25 00:15:40 -0500

    Create employees

--- src/main/java/nzhengs/scheduler/employee/Employee.java
+++ src/main/java/nzhengs/scheduler/employee/Employee.java
@@ -14,7 +14,6 @@ public class Employee {
     private String lastName;
     private String address1;
     private String address2;
-    private String city;
     private String state;
     private String zip;
     private Gender gender;
