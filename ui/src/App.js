2f99915787e070db34cdf044fca97dd90a14850c
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2019-08-27 19:02:40 -0500

    Set up ui build tasks

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,22 +1,16 @@
-import React , {Component} from 'react';
+import React from 'react';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from "./components/navbar/nav"
 import TableList from "./components/table/table"
-import Container from "react-bootstrap/Container";
 import FormDetail from "./components/FormDetail/form.js"
-
 
 function App() {
     return <div className="App">
         <Nav/>
-        <br/>
-
         <TableList/>
         <FormDetail/>
-
-
     </div>
 }
 
