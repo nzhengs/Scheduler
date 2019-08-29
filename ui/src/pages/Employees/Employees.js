962c4a4096660723fdb45e8db7324466b96bb642
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2019-08-28 19:37:56 -0500

    Fix cases in imports

--- ui/src/pages/Employees/Employees.js
+++ ui/src/pages/Employees/Employees.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../../components/Navbar/Navbar";
+import Navbar from "../../components/navbar/Navbar";
 import EmployeeList from "../../components/EmployeeList/EmployeeList";
 import Footer from "../../components/Footer/Footer";
 import {Jumbotron} from "react-bootstrap";
