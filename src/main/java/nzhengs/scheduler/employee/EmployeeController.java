64b48a8d7b1c028500643b1027afa2c8cc95a9b4
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2019-08-31 10:53:07 -0500

    Use proxy instead cors and hard coded url

--- src/main/java/nzhengs/scheduler/employee/EmployeeController.java
+++ src/main/java/nzhengs/scheduler/employee/EmployeeController.java
@@ -1,10 +1,21 @@
 package nzhengs.scheduler.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static nzhengs.scheduler.employee.EmployeeController.baseUrl;
 
 @RestController
+@RequestMapping(baseUrl)
 public class EmployeeController {
+
+    static final String baseUrl = "/api/employees";
 
     private final EmployeeRepo employeeRepo;
 
@@ -13,17 +24,17 @@ public class EmployeeController {
         this.employeeRepo = employeeRepo;
     }
 
-    @GetMapping(path = "/employees")
+    @GetMapping
     Iterable<Employee> getEmployees() {
         return employeeRepo.findAll();
     }
 
-    @PostMapping(path = "/employees")
+    @PostMapping
     Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepo.save(employee);
     }
 
-    @PutMapping(path = "/employees/{id}")
+    @PutMapping(path = "/{id}")
     Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
         employee.setId(id);
         return employeeRepo.save(employee);
